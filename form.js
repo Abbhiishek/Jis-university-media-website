c4888ab0d6e6de0f339fba853090961e08fb0bb8
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2021-10-08 09:50:58 +0530

    added script tag to the index.html

--- form.js
+++ form.js
@@ -1,23 +1,24 @@
-
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyAEFQoKKfr3MUV1DoCjJu0P9BdRw7bcLTE",
-  authDomain: "login-demo-aef99.firebaseapp.com",
-  projectId: "login-demo-aef99",
-  storageBucket: "login-demo-aef99.appspot.com",
-  messagingSenderId: "783184235705",
-  appId: "1:783184235705:web:a639c14fffbf98edb65f68",
-  measurementId: "G-N9K4SZ3R3Y"
+  apiKey: "AIzaSyCHwWx3UGwa4siu23mWMp6nbKoC3pztEsI",
+  authDomain: "jis-university.firebaseapp.com",
+  projectId: "jis-university",
+  storageBucket: "jis-university.appspot.com",
+  messagingSenderId: "626047232746",
+  appId: "1:626047232746:web:9cdb26ec52ba8825f20593",
+  measurementId: "G-5DV440BC8M"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
   //signup function
   function signUp(){
     var email = document.getElementById("email");
