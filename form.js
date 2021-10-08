1e874bd654221c01fd60713ca6c1cb0e08f2aa2a
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2021-10-08 09:52:22 +0530

    done

--- form.js
+++ form.js
@@ -1,25 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-  apiKey: "AIzaSyCHwWx3UGwa4siu23mWMp6nbKoC3pztEsI",
-  authDomain: "jis-university.firebaseapp.com",
-  projectId: "jis-university",
-  storageBucket: "jis-university.appspot.com",
-  messagingSenderId: "626047232746",
-  appId: "1:626047232746:web:9cdb26ec52ba8825f20593",
-  measurementId: "G-5DV440BC8M"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-  //signup function
+//signup function
   function signUp(){
     var email = document.getElementById("email");
     var password = document.getElementById("password");
