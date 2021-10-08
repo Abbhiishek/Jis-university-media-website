78a3a481924c7efc0e720cd4729ea6b6013bdcaa
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2021-10-08 10:28:49 +0530

    resolve some deploy issues

--- form.js
+++ form.js
@@ -1,3 +1,20 @@
+ // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1-7QDAYfcdEhZRdLDytR0eLuSyfVjqsk",
+    authDomain: "form-fb2df.firebaseapp.com",
+    projectId: "form-fb2df",
+    storageBucket: "form-fb2df.appspot.com",
+    messagingSenderId: "490852859264",
+    appId: "1:490852859264:web:874b17b024744fa0bc5565",
+    measurementId: "G-3GGRW9YGKY"
+    };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
+
 //signup function
   function signUp(){
     var email = document.getElementById("email");
