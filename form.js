f570f07f973d11a7294e9fb3fd9981579f239e45
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2021-10-18 16:55:39 +0530

    Add files via upload

--- form.js
+++ form.js
@@ -1,101 +1,63 @@
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyB1-7QDAYfcdEhZRdLDytR0eLuSyfVjqsk",
-    authDomain: "form-fb2df.firebaseapp.com",
-    projectId: "form-fb2df",
-    storageBucket: "form-fb2df.appspot.com",
-    messagingSenderId: "490852859264",
-    appId: "1:490852859264:web:874b17b024744fa0bc5565",
-    measurementId: "G-3GGRW9YGKY"
+    apiKey: "AIzaSyCH3BK4qK6EG039Dm2K0Khzl9bLzo1lC5g",
+    authDomain: "login-signup-form-5cb45.firebaseapp.com",
+    projectId: "login-signup-form-5cb45",
+    storageBucket: "login-signup-form-5cb45.appspot.com",
+    messagingSenderId: "1047209836294",
+    appId: "1:1047209836294:web:b66189eff3c4355b0e2887",
+    measurementId: "G-NXQF2WW747"
 };
-
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+ firebase.initializeApp(firebaseConfig);
+ firebase.analytics();
 
+var datab  = firebase.database().ref('login-data');
+function SignUp(){
+    var email = document.getElementById('eemail');
+    var password = document.getElementById('ppassword');
+    firebase.auth().createUserWithEmailAndPassword(email.value, password.value).then(function(){
+    
+    }).catch(function (error){
+        var errorcode = error.code;
+        var errormsg = error.message;
+    });
+    }
 const auth = firebase.auth();
-
-//signup function
-function signUp() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-
-    const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
-    //
-    promise.catch(e => alert(e.message));
-    alert("SignUp Successfully");
-}
-
-//signIN function
-function signIn() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+function SignIn(){
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
     const promise = auth.signInWithEmailAndPassword(email.value, password.value);
     promise.catch(e => alert(e.message));
-    alert("SignIn Successfully");
-
+    alert("SignIn Successfully"+ email); 
 }
-
-
+document.getElementById('createAccount').addEventListener('submit', (e) => {
+    e.preventDefault();
+    var userInfo = datab.push();
+    userInfo.set({
+        name: getId('Username'),
+        email : getId('eemail'),
+        password : getId('ppassword')
+    });
+    alert("Successfully Signed Up");
+    console.log("Successfully Signed Up");
+    document.getElementById('form').reset();
+});
+function  getId(id){
+    return document.getElementById(id).value;
+}
 //signOut
-
-function signOut() {
-    auth.signOut();
+function SignOut() {
+    auth().signOut();
     alert("SignOut Successfully from System");
 }
-
 //active user to homepage
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         var email = user.email;
         alert("Active user " + email);
+        location.replace("home.html")
 
     } else {
         alert("No Active user Found")
     }
 })
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContent = message;
-    messageElement.classList.remove("form__message--success", "form__message--error");
-    messageElement.classList.add(`form__message--${type}`);
-}
-
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form__input--error");
-    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    const loginForm = document.querySelector("#login");
-    const createAccountForm = document.querySelector("#createAccount");
-
-    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-    });
-
-    document.querySelector("#linkLogin").addEventListener("click", e => {
-        e.preventDefault();
-        loginForm.classList.remove("form--hidden");
-        createAccountForm.classList.add("form--hidden");
-    });
-
-    loginForm.addEventListener("submit", e => {
-        e.preventDefault();    
-    });
-
-    document.querySelectorAll(".form__input").forEach(inputElement => {
-       
-        inputElement.addEventListener("input", e => {
-            clearInputError(inputElement);
-        });
-    });
-});
